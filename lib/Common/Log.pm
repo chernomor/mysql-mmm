7b44f9993069393faa441327f17e80dede24f02d
mail@pascalhofmann.de <>
2008-12-29 09:56:08 +0000

    ph: Keep default log files and files with relative paths in SELF_DIR

--- lib/Common/Log.pm
+++ lib/Common/Log.pm
@@ -3,12 +3,14 @@ package MMM::Common::Log;
 use strict;
 use warnings FATAL => 'all';
 use Log::Log4perl qw(:easy);
+use English qw( PROGRAM_NAME );
 
 our $VERSION = '0.01';
 
 
-sub init($) {
+sub init($$) {
 	my $file = shift;
+	my $progam = shift;
 
 	my @paths = qw(/etc /etc/mmm /etc/mysql-mmm);
 	push @paths, $main::SELF_DIR if defined($main::SELF_DIR);
@@ -29,39 +31,39 @@ sub init($) {
 	}
 
 	# Use default configuration
-	my $conf = q(
+	my $conf = "
 		log4perl.logger = INFO, FileInfo, FileWarn, FileError, FileFatal, MailFatal
 
 		log4perl.appender.FileInfo            = Log::Log4perl::Appender::File
-		log4perl.appender.FileInfo.filename   = mmmd_mon.info
+		log4perl.appender.FileInfo.filename   = $main::SELF_DIR/$progam.info
 		log4perl.appender.FileInfo.layout     = SimpleLayout
 		log4perl.appender.FileInfo.Threshold  = INFO 
 		log4perl.appender.FileInfo.recreate   = 1
 
 		log4perl.appender.FileWarn            = Log::Log4perl::Appender::File
-		log4perl.appender.FileWarn.filename   = mmmd_mon.warn
+		log4perl.appender.FileWarn.filename   = $main::SELF_DIR/$progam.warn
 		log4perl.appender.FileWarn.layout     = SimpleLayout
 		log4perl.appender.FileWarn.Threshold  = WARN 
 		log4perl.appender.FileWarn.recreate   = 1
 
 		log4perl.appender.FileError           = Log::Log4perl::Appender::File
-		log4perl.appender.FileError.filename  = mmmd_mon.error
+		log4perl.appender.FileError.filename  = $main::SELF_DIR/$progam.error
 		log4perl.appender.FileError.layout    = SimpleLayout
 		log4perl.appender.FileError.Threshold = ERROR 
 		log4perl.appender.FileError.recreate  = 1
 
 		log4perl.appender.FileFatal           = Log::Log4perl::Appender::File
-		log4perl.appender.FileFatal.filename  = mmmd_mon.fatal
+		log4perl.appender.FileFatal.filename  = $main::SELF_DIR/$progam.fatal
 		log4perl.appender.FileFatal.layout    = SimpleLayout
 		log4perl.appender.FileFatal.Threshold = FATAL 
 		log4perl.appender.FileFatal.recreate  = 1
 
 		log4perl.appender.MailFatal           = Log::Dispatch::Email::MailSend
 		log4perl.appender.MailFatal.to        = root
-		log4perl.appender.MailFatal.subject   = FATAL error in MMM Monitor
+		log4perl.appender.MailFatal.subject   = FATAL error in $progam
 		log4perl.appender.MailFatal.layout    = SimpleLayout
 		log4perl.appender.MailFatal.Threshold = FATAL 
-	);
+	";
 	Log::Log4perl->init(\$conf);
 
 }

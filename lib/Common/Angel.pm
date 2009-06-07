9a2d31bd73dfe6c14b63acbd90d6a40bf93c0cea
Pascal Hofmann <mail@pascalhofmann.de>
2009-06-07 23:31:36 +0200

    -Added angel functionality -Added documentation for ssh_port and ssh_parameters -released version 2.0.5

---
+++ lib/Common/Angel.pm
@@ -0,0 +1,77 @@
+package MMM::Common::Angel;
+
+use strict;
+use warnings FATAL => 'all';
+use English qw( CHILD_ERROR ERRNO );
+use Log::Log4perl qw(:easy);
+use Errno qw( EINTR );
+use POSIX qw( WIFEXITED WIFSIGNALED WEXITSTATUS WTERMSIG WNOHANG );
+
+
+our $start_process;
+our $pid;
+
+sub Init($) { 
+	my $pidfile = shift;
+
+	
+	$MMM::Common::Angel::start_process	= 1;
+	my $is_shutdown	= 0;
+
+	$pidfile->create() if (defined($pidfile));
+
+	$SIG{INT}	= \&MMM::Common::Angel::SignalHandler;
+	$SIG{TERM}	= \&MMM::Common::Angel::SignalHandler;
+	$SIG{QUIT}	= \&MMM::Common::Angel::SignalHandler;
+
+	do {
+
+		if ($MMM::Common::Angel::start_process) {
+			$MMM::Common::Angel::start_process = 0;
+
+			# Create a new child
+			$MMM::Common::Angel::pid = fork();
+
+			# Die if we couldn't fork
+			LOGDIE "Couldn't fork child process." unless (defined($MMM::Common::Angel::pid));
+
+			# Return if we are the child
+			return if ($MMM::Common::Angel::pid == 0);
+		}
+
+		# Wait for child to exit
+		if (waitpid($MMM::Common::Angel::pid, 0) == -1) {
+			if ($ERRNO{ECHLD}) {
+				$is_shutdown = 1 unless ($MMM::Common::Angel::start_process);
+			}
+		}
+		else {
+			if (WIFEXITED($CHILD_ERROR)) {
+				if (WEXITSTATUS($?) == 0) {
+					INFO "Child exited normally (with exitcode 0), shutting down";
+					$is_shutdown = 1;
+				}
+				else {
+					FATAL sprintf("Child exited with exitcode %s, restarting", WEXITSTATUS($?));
+					$MMM::Common::Angel::start_process	= 1;
+				}
+			}
+			if (WIFSIGNALED($CHILD_ERROR)) {
+				FATAL sprintf("Child exited with signal %s, restarting", WTERMSIG($?));
+				$MMM::Common::Angel::start_process	= 1;
+			}
+		}
+
+	} while (!$is_shutdown);
+
+	
+	$pidfile->remove() if (defined($pidfile));
+	exit(0);
+}
+
+sub SignalHandler {
+	my $signame = shift;
+	kill ($signame, $MMM::Common::Angel::pid);
+}
+
+1;
